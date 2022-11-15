99603e23c519f9f7c14fcf33f88064f505955b04
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2022-11-15 15:11:53 -0800

    Add project files.

---
+++ BetterScrapDrops/Mod.cs
@@ -0,0 +1,20 @@
+﻿using PulsarModLoader;
+
+namespace BetterScrapDrops
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "1.0.0";
+
+        public override string Author => "Dragon";
+
+        public override string Name => "Better Scrap Drops";
+
+        public override string LongDescription => "Causes components dropped from destroyed ships to provide higher level scrap when picked up. Level is based on components the ship had.";
+
+        public override string HarmonyIdentifier()
+        {
+            return $"{Author}.{Name}";
+        }
+    }
+}
---
+++ BetterScrapDrops/Patch.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using static PulsarModLoader.Patches.HarmonyHelpers;
+
+namespace BetterScrapDrops
+{
+    [HarmonyPatch(typeof(PLSpaceScrap), "OnCollect")]
+    internal class Patch
+    {
+        //returns level for scrap
+        static int PatchMethod(int componentHash)
+        {
+            if(componentHash == -1)
+            {
+                return 0;
+            }
+            else
+            {
+                return PLShipComponent.CreateShipComponentFromHash(componentHash).Level;
+            }
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> targetSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldc_I4_0),
+                new CodeInstruction(OpCodes.Stloc_3)
+            };
+            List<CodeInstruction> injectedSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(PLSpaceScrap), "SpecificComponent_CompHash")),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Patch), "PatchMethod")),
+                new CodeInstruction(OpCodes.Stloc_3)
+            };
+            return PatchBySequence(instructions, targetSequence, injectedSequence, PatchMode.REPLACE);
+        }
+    }
+}
