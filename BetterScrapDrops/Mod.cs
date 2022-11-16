da750febb232c4defed5b8865afd2fa15f9c8901
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2022-11-15 23:10:43 -0800

    -Updated Description

--- BetterScrapDrops/Mod.cs
+++ BetterScrapDrops/Mod.cs
@@ -10,7 +10,7 @@ namespace BetterScrapDrops
 
         public override string Name => "Better Scrap Drops";
 
-        public override string LongDescription => "Causes components dropped from destroyed ships to provide higher level scrap when picked up. Level is based on components the ship had.";
+        public override string LongDescription => "Causes components dropped from destroyed ships to provide higher level scrap when picked up. Level is based on components the ship had. \n\nAdditionally increases level of dropped scrap from scrapper turrets.";
 
         public override string HarmonyIdentifier()
         {
